cb1f242516ccc321617f92652d9e96df47ff1fba
Alonsobtech <lonsobraithwaite1996@gmail.com>
2024-09-14 00:17:34 -0400

    Edited font size for 600px and 768px view.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navLinks = document.querySelector('.nav-links');
+let menuIcon = document.querySelector('#menu-icon');
+let navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-solid fa-bars');
     navLinks.classList.toggle('active');
 }
