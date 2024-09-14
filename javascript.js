a6e83fb40f2df32cc73669aa883b168b2a6414ee
Alonsobtech <lonsobraithwaite1996@gmail.com>
2024-09-14 01:11:10 -0400

    added missing id's for nav links to work

--- javascript.js
+++ javascript.js
@@ -2,6 +2,6 @@ let menuIcon = document.querySelector('#menu-icon');
 let navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-solid fa-bars');
+    menuIcon.classList.toggle('fa-solid');
     navLinks.classList.toggle('active');
 }
