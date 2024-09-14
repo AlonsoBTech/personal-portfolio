cfdb44b979adb11d0182cc7c85ad1b4666eb7f45
Alonsobtech <lonsobraithwaite1996@gmail.com>
2024-09-14 19:21:43 -0400

    Refactor media query for mobile view

--- javascript.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navLinks = document.querySelector('.nav-links');
-
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('active');
-    navLinks.classList.toggle('active');
-})
